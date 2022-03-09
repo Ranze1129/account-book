9d193fb625f4d1e7bffc880be92c38f665de3584
Ranze <1066467243@qq.com>
2022-03-09 18:10:31 +0800

    创建 <Icon />

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import router from './router';
 import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
 Vue.config.productionTip = false;
 
 Vue.component('Nav', Nav);
 Vue.component('Layout', Layout)
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+Vue.component('Icon', Icon)
 
 new Vue({
   router,
