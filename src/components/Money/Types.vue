6cee667844fe1d79adbf270aa78b1fbb0e69d8e7
Ranze <1066467243@qq.com>
2022-03-11 17:44:03 +0800

    基本完成numberPad组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -14,7 +14,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-
 @Component
 export default class Types extends Vue {
   type = '-';
@@ -26,26 +25,6 @@ export default class Types extends Vue {
     this.type = type;
   }
 }
-// export default {
-//   name: 'Types',
-//   props: ['xxx'],
-//   data() {
-//     return {
-//       type: '-' // '-'表示支出，'+'表示收入
-//     }
-//   },
-//   mounted() {
-//     console.log(this.xxx)
-//   },
-//   methods: {
-//     selectType(type) { // type 只能是 '-' 和 '+' 中的一个
-//       if (type !== '-' && type !== '+') {
-//         throw new Error('type is unknown')
-//       }
-//       this.type = type
-//     }
-//   }
-// }
 </script>
 
 <style lang="scss" scoped>
