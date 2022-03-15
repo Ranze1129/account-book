42176d95d823ce8ae6b0e94ea34f7d459caae7d7
Ranze <1066467243@qq.com>
2022-03-15 23:43:20 +0800

    解决 id 生成问题

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -48,8 +48,11 @@ export default class EditLabel extends Vue {
 
   remove() {
     if (this.tag) {
-      tagListModel.remove(this.tag.id);
-      this.$router.back()
+      if(tagListModel.remove(this.tag.id)) {
+        this.$router.back()
+      }else{
+        window.alert('删除失败')
+      }
     }
   }
   goBack(){
