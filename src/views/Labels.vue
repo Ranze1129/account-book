e740487de2dc8b05ee1d77582b0a8d77584500d3
Ranze <1066467243@qq.com>
2022-03-15 17:47:51 +0800

    添加 EditLabel.vue的样式，抽离了两个组件Button和FormItem

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -8,7 +8,7 @@
        </router-link>
      </div>
      <div class="createTag-wrapper">
-       <button class="createTag" @click="createTag">新建标签</button>
+       <Button class="createTag" @click="createTag">新建标签</Button>
      </div>
    </Layout>
 </template>
@@ -17,9 +17,12 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
+import Button from '@/components/Button.vue';
 
 tagListModel.fetch();
-@Component
+@Component({
+  components: {Button}
+})
 export default class Labels extends Vue {
   tags = tagListModel.data;
 
@@ -61,13 +64,6 @@ export default class Labels extends Vue {
 }
 
 .createTag {
-  background: #767676;
-  color: white;
-  border-radius: 4px;
-  border: none;
-  height: 40px;
-  padding: 0 16px;
-
   &-wrapper {
     text-align: center;
     padding: 16px;
