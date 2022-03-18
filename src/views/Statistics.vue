af2671955ac7dfad539290bbaf67a161cc3a9882
Ranze <1066467243@qq.com>
2022-03-18 17:30:55 +0800

    Statistics.vue 完成数据提取

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,31 +3,25 @@
     <Tabs class-prefix="type"
           :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-<div>
-  type:{{type}}
-  <br/>
-  interval:{{interval}}
-</div>
+    <div>
+      type:{{ type }}
+      <br/>
+      interval:{{ interval }}
+    </div>
+    <div>
+      <ol>
+        <li v-for="(group,index) in result" :key="index">
+          <h3>{{group.title}}</h3>
+          <ol>
+            <li v-for="item in group.items" :key="item.id">
+              {{ item.amount }} {{ item.createAt }}
+            </li>
+          </ol>
+        </li>
+      </ol>
+    </div>
   </Layout>
 </template>
-
-<style scoped lang="scss">
-::v-deep .type-tabs-item {
-  background: white;
-
-  &.selected {
-    background: #c4c4c4;
-
-    &::after {
-      display: none;
-    }
-  }
-}
-
-::v-deep .interval-tabs-item {
-  height: 48px;
-}
-</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -40,10 +34,54 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    type HashTableValue = {title: string, items:RecordItem[]}
+
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items:[]};
+      hashTable[date].items.push(recordList[i]);
+    }
+    console.log(hashTable);
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
   recordTypeList = recordTypeList;
 };
 </script>
+
+<style scoped lang="scss">
+::v-deep {
+  .type-tabs-item {
+    background: white;
+
+    &.selected {
+      background: #c4c4c4;
+
+      &::after {
+        display: none;
+      }
+    }
+  }
+
+  .interval-tabs-item {
+    height: 48px;
+  }
+}
+
+
+</style>
 
