c5b0d9116bd5c5ed87faf18016e2ffc9d526746e
Ranze <1066467243@qq.com>
2022-03-18 17:52:30 +0800

    完成列表的CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,24 +4,44 @@
           :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <div>
-      type:{{ type }}
-      <br/>
-      interval:{{ interval }}
     </div>
-    <div>
       <ol>
         <li v-for="(group,index) in result" :key="index">
-          <h3>{{group.title}}</h3>
+          <h3 class="title">{{ group.title }}</h3>
           <ol>
-            <li v-for="item in group.items" :key="item.id">
-              {{ item.amount }} {{ item.createAt }}
+            <li v-for="item in group.items" :key="item.id"
+                class="record">
+              <span>{{tagString(item.tags)}}</span>
+              <span class="notes">{{item.notes}}</span>
+             <span>￥{{ item.amount }} </span>
             </li>
           </ol>
         </li>
       </ol>
-    </div>
   </Layout>
 </template>
+
+<style scoped lang="scss">
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  @extend %item;
+  background: white;
+}
+.notes{
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -34,6 +54,9 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]){
+    return tags.length=== 0 ? '无': tags.join(',')
+  }
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
