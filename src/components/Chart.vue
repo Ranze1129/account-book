0356130d4011d5dea2265a9c9155bf061d16c7fd
Ranze <1066467243@qq.com>
2022-04-01 15:32:58 +0800

    自己封装 Chart 组件

---
+++ src/components/Chart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper" ref="wrapper">chart</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import * as eCharts from 'echarts';
+
+@Component
+export default class Chart extends Vue {
+  @Prop() options: any;
+  mounted(){
+    if(this.options === undefined){
+      return console.error('options 为空')
+    }
+    const chart = eCharts.init(this.$refs.wrapper as HTMLDivElement)
+    chart.setOption(this.options)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+  height: 400px;
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,19 +2,20 @@
   <Layout>
     <Tabs class="tabs" class-prefix="type"
           :data-source="recordTypeList" :value.sync="type"/>
-      <ol v-if="groupedList.length>0">
-        <li v-for="(group,index) in groupedList" :key="index">
-          <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id"
-                class="record">
-              <span>{{ tagString(item.tags) }}</span>
-              <span class="notes">{{ item.notes }}</span>
-              <span>￥{{ item.amount }} </span>
-            </li>
-          </ol>
-        </li>
-      </ol>
+    <Chart class="chart" :options="x"/>
+    <ol v-if="groupedList.length>0">
+      <li v-for="(group,index) in groupedList" :key="index">
+        <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }} </span>
+          </li>
+        </ol>
+      </li>
+    </ol>
     <div v-else class="noResult">
       目前没有相关记录
     </div>
@@ -30,10 +31,12 @@ import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
 
 
 @Component({
-  components: {Tabs}
+  name: 'HelloWorld',
+  components: {Tabs, Chart},
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
@@ -54,6 +57,38 @@ export default class Statistics extends Vue {
     } else {
       return day.format('YYYY年M月D日');
     }
+  }
+
+  get x() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: [
+          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      tooltip: {
+        show: true,
+        triggerOn: 'click'
+      },
+      series: [
+        {
+          data: [
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200, 150, 80, 70, 110, 130,
+            120, 200
+          ],
+          type: 'line',
+        }
+      ]
+    };
   }
 
   get recordList() {
@@ -101,6 +136,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
+.chart {
+  max-width: 100%;
+  height: 400px;
+}
 .noResult {
   padding: 16px;
   text-align: center;
